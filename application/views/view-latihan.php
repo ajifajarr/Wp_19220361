973c02f955178dc0956e937270bc00397a808bef
ajifajarr <adjiefajarr@gmail.com>
2023-10-13 15:14:46 +0700

    Menambahkan Tugas

---
+++ application/views/view-latihan.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Latihan1</title>
+</head>
+
+<body>
+    Halo kawan..Yuk Kita Belajar Web Programming..!!!<br>
+    Nilai 1 =<?= $nilai1;?>
+    Nilai 2 =<?= $nilai1;?>
+    Ini hasil dari pemodelan dengan methode penjumlahan yaitu <?= $nilai1."+".$nilai2."=".$hasil; ?>
+    
+</body>
+</html>
+   
